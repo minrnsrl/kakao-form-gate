b38b3e894dc34b590fba9173f8daf22c9b040980
minrnsrl <minrnsrl@gmail.com>
2025-08-13 15:37:40 +0900

    Update reply.ts

--- lib/reply.ts
+++ lib/reply.ts
@@ -1,1 +1,10 @@
+export function reply(text: string) {
+  return {
+    version: "2.0",
+    template: {
+      outputs: [{ simpleText: { text } }],
+      quickReplies: [{ action: "message", label: "인증 시작", messageText: "인증 시작" }],
+    },
+  };
+}
 
